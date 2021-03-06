76a12b03b0880fd5d6440cd9357a553e56e4fe29
Patrick Karney <patrickkarney@gmail.com>
2021-03-06 11:44:48 +0100

    Add modals and chart

--- js/script.js
+++ js/script.js
@@ -3,17 +3,85 @@ function burgerToggle() {
     const burgerElement = document.querySelector('.btn-sidebar');
     console.log('tutaj', burgerElement);
     //get Id of sidebar nav list element
-    const sidebarList = document.getElementById('sidebar-nav');
+    const sidebarList = document.getElementById('sidebar');
     //add eventlistener to burger
     burgerElement.addEventListener('click', function(event){
         event.preventDefault();
-        if(sidebarList.style.display === 'none'){
-            sidebarList.style.display = 'inline-block';
+        if(sidebarList.style.height === '60px'){
+            sidebarList.style.height = '100%';
         }
         else{
-            sidebarList.style.display = 'none';
+            sidebarList.style.height = '60px';
         }
     })
 }
 
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  }
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault()
+        closeModal()
+    })
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  })
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  })
+
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+  }
+
+
+openModal (myModal);
 burgerToggle();
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            // 7
+            hidden: true,
+        }]
+    },
+});
